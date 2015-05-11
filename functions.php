db85a1d1d489e4063ace78be7d9d83e018cec7cf
Jami Gibbs <jami0821@gmail.com>
2015-05-11 11:59:15 -0500

    Update stylesheet parent handle, closes #1

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function enqueue_parent_theme_style() {
     wp_enqueue_style( 'heartfelt-parent-style', get_template_directory_uri().'/style.css' );
-    wp_dequeue_style( 'rescue_style' );
+    wp_dequeue_style( 'heartfelt-styles' );
     wp_enqueue_style( 'heartfelt-child-style', get_stylesheet_directory_uri().'/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style', 99 );
